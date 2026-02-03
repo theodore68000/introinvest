d994b622db0385d72501d190cff72c4cf0006fd3
Theodore <theodore.matter@student-cs.fr>
2026-02-03 08:59:53 +0100

    pour que ca marche

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/introinvest",   // nom exact de ton repo
+  images: {
+    unoptimized: true
+  }
+};
+
+module.exports = nextConfig;
+
+
