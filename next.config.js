692bfc54df18444714c9ae50f378043bd38bed1a
Theodore <theodore.matter@student-cs.fr>
2026-02-03 09:11:03 +0100

    inchallah c bon

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  distDir : "docs",
   basePath: "/introinvest",   // nom exact de ton repo
   images: {
     unoptimized: true
